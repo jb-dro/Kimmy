3c48cdb7a3016a9b6b3e4ab606ce26965533d885
Jack <jbrowntown139@gmail.com>
2023-04-14 14:01:00 -0500

    exit!

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ root_button = split_button.SplitButton(root, "horizontal")
 # score_box = tk.Label(root, text="Score: ", font=("Helvetica", 16))
 # score_box.pack(side=tk.TOP, anchor=tk.NE)
 
-# add exit button to the top right corner
+# add a red exit button to the top right corner
 exit_button = tk.Button(root, text="Exit", command=root.destroy)
 exit_button.pack(side=tk.TOP, anchor=tk.NE)
 
